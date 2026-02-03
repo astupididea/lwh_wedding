73ea8a70c2fc63f8199b8990ab4d409a68aacaef
lee <astupididea20@gmail.com>
2026-02-03 20:04:50 +0900

    eight commit

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 const image1 = document.querySelector(".card .image1");
 const image2 = document.querySelector(".card .image2");
 
-image1.addEventListener('mouseenter', function(){
-    image2.style.display = "block";
-});
 
-image1.addEventListener('mouseleave', function(){
-    image2.style.display = "none";
-});
+function toggleImage() {
+    setTimeout(function(){
+        image2.style.opacity = "1";
+        // image2.style.display = "block";
+        
+        setTimeout(function(){
+            image2.style.opacity = "0";
+            // image2.style.display = "none";
+            
+            // 다시 반복
+            toggleImage();
+        }, 2500);
+        
+    }, 2500);
+}
+
+toggleImage();
+
+// 타임아웃 한번 반복
+// setTimeout(function(){
+//     image2.style.display = "block";
+
+//     setTimeout(function(){
+//     image2.style.display = "none";
+
+//     }, 3000);
+
+// }, 6000);
+
+// 이벤트 리스너
+// image1.addEventListener('mouseenter', function(){
+//     image2.style.display = "block";
+// });
+
+// image1.addEventListener('mouseleave', function(){
+//     image2.style.display = "none";
+// });
 
 
 // 계좌번호
